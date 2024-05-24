93b5336dc6c1110c4d90e7f28022dd282744f50f
NaveenBupati <navinbhupathi22@gmail.com>
2024-05-24 17:02:27 +0530

    Added

--- src/vendorDashboard/components/SideBAr.jsx
+++ src/vendorDashboard/components/SideBAr.jsx
@@ -31,7 +31,7 @@ const SideBar = ({ showFirmHandler, showProductHandler, showAllProductsHandler, 
     return (
         <>
             {!isSidebarOpen && (
-                <div style={{ marginTop:"30px",  position: "sticky", top: "60px", left: "20px", zIndex: "9999" }}>
+                <div style={{ marginTop:"60px", position: "fixed", top: "60px", left: "100px", zIndex: "9999" }}>
                 <FontAwesomeIcon 
                     icon={faBars} 
                     onClick={handleSidebarToggle} 
